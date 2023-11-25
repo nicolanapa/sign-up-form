616d182af4ff6d39d592cf482203e26279d97dbb
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2023-11-25 20:55:25 +0100

    Changes

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,23 +1,59 @@
-let password1 = "";
-let password2 = "";
-
-function password_check() {
-    let password = document.querySelector("#password");
-    let password_repeat = document.querySelector("#password_repeat");
-    password.addEventListener("keypress", function(e) {
-    return password1 = password;
-    });
-    password_repeat.addEventListener("keypress", function(e) {
-    return password2 = password_repeat;
-    });
-    if (password1 == password2) {
-        console.log(password1);
-        console.log(password2);
-    }
-    else {
-        console.log(password1);
-        console.log(password2);
+/*function password_updated1() {
+    password1 = password.value;
+    if ( password1 != "" && password2 != "") {
+        password_check();
     }
 }
+function password_updated2() {
+    password2 = password_repeat.value;
+    if ( password1 != "" && password2 != "") {
+        password_check();
+    }
+}*/
+/*function passowrd_nonvuoto() {
+    if ( password1 != "" && password2 != "" ) {
+        password_check();
+    }
+}
+function password_updated() {
+    if ( password.value != "" ) {
+        password1 = password.value;
+        passowrd_nonvuoto();
+    }
+    else if ( password_repeat.value != "" ) {
+        password2 = password_repeat.value;
+        passowrd_nonvuoto();
+    }
+}
+function password_check() {
+        if (password1 == password2) {
+            console.log(password1 + " true");
+            console.log(password2 + " true");
+        }
+        else {
+            console.log(password1 + " false");
+            console.log(password2 + " false");
+        }
+}*/
+/*function password_updated() {
+    password_repeat.addEventListener("input", function(e) {
+    password1 = password.value;
+    password2 = password_repeat.value;
+});
+    password_check();
+}*/
+let password = document.querySelector("#password");
+let password_repeat = document.querySelector("#password_repeat");
+password_repeat.addEventListener("input", function(e) {
+    let password1 = document.querySelector("#password").value;
+    let password2 = document.querySelector("#password_repeat").value;
 
-
+    if (password1 == password2) {
+        console.log(password1 + " true");
+        console.log(password2 + " true");
+    }
+    else {
+        console.log(password1 + " false");
+        console.log(password2 + " false");
+    }
+});
